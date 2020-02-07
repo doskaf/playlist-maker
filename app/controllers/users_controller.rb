556df0044d0104cff1d4a9e3e04bbb8428ae4152
Dominique Skaf <doskaf@yahoo.com>
2020-02-07 10:03:27 -0600

    Created routes for UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,44 @@
 class UsersController < ApplicationController
 
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            sessions[:user_id] = @user.id
+            redirect_to @user
+        else
+            render :new
+        end
+    end
+
+    def show
+        @user = User.find_by_id(params[:id])
+    end
+
+    def edit
+        @user = User.find_by_id(params[:id])
+    end
+
+    def update
+        @user = User.find_by_id(params[:id])
+        @user.update(user_params)
+        @user.save ? (redirect_to @user) : (render :new)
+    end
+
+    def destroy
+        @user = User.find_by_id(params[:id])
+        @user.destroy
+        flash[:notice] = "User deleted."
+        redirect_to "/"
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
 end
