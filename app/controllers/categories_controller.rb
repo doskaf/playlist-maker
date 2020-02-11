33728d04f9cc30697a104e04820260afb3288b77
Dominique Skaf <doskaf@yahoo.com>
2020-02-11 16:18:56 -0600

    Created routes and views for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,11 @@
 class CategoriesController < ApplicationController
 
+    def show
+        @category = Category.find_by_id(params[:id])
+    end
+
+    def index
+        @categories = Category.all
+    end
+
 end
