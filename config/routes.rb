d1f1b1e6c5c2879b5d37b75a8e4eb996ae50762e
Dominique Skaf <doskaf@yahoo.com>
2020-02-13 10:42:44 -0600

    Set up sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "welcome#home"
   resources :users
-  delete '/users/:id', to: 'users#destroy', as:'user'
+  get '/signup', to: 'users#new'
+  delete '/users/:id', to: 'users#destroy'
   resources :playlists
-  delete '/playlists/:id', to: 'playlists#destroy', as:'playlist'
+  delete '/playlists/:id', to: 'playlists#destroy'
   resources :songs
   resources :categories
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
 end
