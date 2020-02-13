56446b164dde6666efc33939a4e79efa3adb0a15
Dominique Skaf <doskaf@yahoo.com>
2020-02-13 11:13:41 -0600

    Created sessions actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,21 @@
 class SessionsController < ApplicationController
     
-    
+    def new
+    end
+ 
+    def create
+        @user = User.find_by(username: params[:username])
+        if !@user.authenticate(params[:password])
+            flash[:alert] = "Incorrect Password."
+            render :new
+        else
+            session[:user_id] = @user.id
+            redirect '/categories'
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+    end
 
 end
