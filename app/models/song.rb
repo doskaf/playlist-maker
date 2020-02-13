dde1bc0a77711c3a78d745171b3a19370354a880
Dominique Skaf <doskaf@yahoo.com>
2020-02-13 11:54:56 -0600

    Added validations to classes

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,5 @@
 class Song < ApplicationRecord
+    validates :title, :artist, presence: true
     has_many :playlist_songs
     has_many :playlists, through: :playlist_songs
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :username, :password, presence: true
     validates_uniqueness_of :username
     has_many :playlists
     has_many :categories, through: :playlists
