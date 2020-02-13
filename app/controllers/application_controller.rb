5cede8ac71221ad4945f0eb5e95d26bc680ef39d
Dominique Skaf <doskaf@yahoo.com>
2020-02-13 14:15:43 -0600

    Added navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,2 @@
 class ApplicationController < ActionController::Base
-
-    helpers do
-
-        def current_user
-            @current_user ||= User.find_by(:id => session[:id]) if session[:id]
-        end
-
-        def logged_in?
-            !!current_user
-        end
-
-        def authenticate
-            redirect "/login" if !logged_in?
-        end
-
-        def login(username, password)
-            user = User.find_by(:username => username)
-            if  user && user.authenticate(password)
-                session[:id] = user.id
-            else
-                redirect '/login'
-            end
-        end
-
-    end
-
 end
