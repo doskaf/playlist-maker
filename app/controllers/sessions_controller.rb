355ebbe3746af605e7bb2fe39c488faa97032b3b
Dominique Skaf <doskaf@yahoo.com>
2020-02-14 10:37:06 -0600

    Added delete option for users and playlists only if correct user is logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,17 +5,18 @@ class SessionsController < ApplicationController
  
     def create
         @user = User.find_by(username: params[:username])
-        if !@user.authenticate(params[:password])
+        if @user && @user.authenticate(params[:password])
+            session[:id] = @user.id
+            redirect_to '/categories'
+        else
             flash[:alert] = "Incorrect Password."
             render :new
-        else
-            session[:user_id] = @user.id
-            redirect '/categories'
         end
     end
 
     def destroy
-        session.delete :user_id
+        session.delete :id
+        redirect_to '/'
     end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,14 @@ module ApplicationHelper
         !!current_user
     end
 
+    def correct_user?
+        (logged_in?) && (current_user == User.find_by_id(params[:id]))
+    end
+
+    def playlist_owner?
+        (logged_in?) && (current_user == Playlist.find_by_id(params[:id]).user)
+    end
+
     def authenticate
         redirect "/login" if !logged_in?
     end
