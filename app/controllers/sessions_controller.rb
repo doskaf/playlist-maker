f3045516010adcca50775b9e566d90daacf3e626
Dominique Skaf <doskaf@yahoo.com>
2020-02-15 23:50:34 -0600

    Setting up login through facebook

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,23 @@ class SessionsController < ApplicationController
     end
  
     def create
-        @user = User.find_by(username: params[:username])
-        if @user && @user.authenticate(params[:password])
-            session[:id] = @user.id
-            redirect_to '/categories'
+        if params[:username]
+            @user = User.find_by(username: params[:username]) if params[:username]
+            if @user && @user.authenticate(params[:password])
+                session[:id] = @user.id
+                redirect_to '/categories'
+            else
+                flash[:alert] = "Incorrect Password."
+                render :new
+            end
         else
-            flash[:alert] = "Incorrect Password."
-            render :new
+            @user = User.find_or_create_by(uid: auth['uid']) do |u|
+                u.name = auth['info']['name']
+                u.email = auth['info']['email']
+                u.image = auth['info']['image']
+            end
+            session[:user_id] = @user.id
+            redirect_to '/categories'
         end
     end
 
@@ -18,5 +28,11 @@ class SessionsController < ApplicationController
         session.delete :id
         redirect_to '/'
     end
+
+    private
+ 
+  def auth
+    request.env['omniauth.auth']
+  end
 
 end
---
+++ db/migrate/20200216042228_add_more_columns_to_users.rb
@@ -0,0 +1,10 @@
+class AddMoreColumnsToUsers < ActiveRecord::Migration[6.0]
+  
+  def change
+    add_column :users, :name, :string
+    add_column :users, :email, :string
+    add_column :users, :image, :string
+    add_column :users, :uid, :string
+  end
+
+end
