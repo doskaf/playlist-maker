6e7293d8f1810d287568d7179e38a9ef248e5e6f
Dominique Skaf <doskaf@yahoo.com>
2020-02-19 14:57:19 -0600

    Added helper methods to make create method clearer/easier to read

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,28 +5,9 @@ class SessionsController < ApplicationController
 
     def create
         if auth_hash = request.env["omniauth.auth"]
-            oauth_nickname = request.env["omniauth.auth"]["info"]["nickname"]
-            if @user = User.find_by(:username => oauth_nickname)
-                session[:id] = @user.id
-                redirect_to '/'
-            else
-                @user = User.new(:username => oauth_nickname, :password => SecureRandom.hex)
-                if @user.save
-                    session[:id] = @user.id
-                    redirect_to '/'
-                else
-                    raise @user.errors.full_messages.inspect
-                end
-            end
+           omniauth_login
         else
-            @user = User.find_by(username: params[:username]) if params[:username]
-            if @user && @user.authenticate(params[:password])
-                session[:id] = @user.id
-                redirect_to '/'
-            else
-                flash[:alert] = "Incorrect Password."
-                render :new
-            end
+            regular_login
         end
     end
 
@@ -39,6 +20,33 @@ class SessionsController < ApplicationController
  
   def auth
     request.env['omniauth.auth']
+  end
+
+  def omniauth_login
+    oauth_nickname = request.env["omniauth.auth"]["info"]["nickname"]
+    if @user = User.find_by(:username => oauth_nickname)
+        session[:id] = @user.id
+        redirect_to '/'
+    else
+        @user = User.new(:username => oauth_nickname, :password => SecureRandom.hex)
+        if @user.save
+            session[:id] = @user.id
+            redirect_to '/'
+        else
+            raise @user.errors.full_messages.inspect
+        end
+    end
+  end
+
+  def regular_login
+    @user = User.find_by(username: params[:username]) if params[:username]
+    if @user && @user.authenticate(params[:password])
+        session[:id] = @user.id
+        redirect_to '/'
+    else
+        flash[:alert] = "Incorrect Password."
+        render :new
+    end
   end
 
 end
